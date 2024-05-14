142d7eebe1ca3e913bbbb54e1ef239e522fb13b5
DoganHaluk <ha_look@yahoo.com>
2024-05-14 10:09:00 +0200

    SchoolClassCourseEditor created

---
+++ LMSApi/LMSApi/Services/SchoolClassCourseEditor.cs
@@ -0,0 +1,29 @@
+﻿using LMSBase.Models.Domain;
+using LMSBase.Models.Dtos;
+
+namespace LMSApi.Services
+{
+	public class SchoolClassCourseEditor
+	{
+		private readonly SchoolClassCourseService _schoolClassCourseService;
+
+		public SchoolClassCourseEditor(SchoolClassCourseService schoolClassCourseService)
+		{
+			_schoolClassCourseService = schoolClassCourseService;
+		}
+
+		public void CreateSchoolClassCourse(int schoolClassId, List<int> courseIds)
+		{
+			foreach (int courseId in courseIds)
+			{
+				SchoolClassCourse schoolClassCourse = new SchoolClassCourse()
+				{
+					CourseId = courseId,
+					SchoolClassId = schoolClassId,
+					StatusId = 1
+				};
+				_schoolClassCourseService.CreateSchoolClassCourse(schoolClassCourse);
+			}
+		}
+	}
+}
